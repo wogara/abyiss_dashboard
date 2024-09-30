d974f5bb2f8e610378a9103285d35992fa9462f2
wogara <weogara@gmail.com>
2024-09-30 16:27:23 -0400

    fix vertical padding

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ export default async function Home() {
   const transactions: Transaction[] = await getTransactions();
 
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-between py-24 px-2 md:px-24">
-      <section className="hero flex flex-col items-center text-center px-6 py-12 md:px-12 lg:py-24">
+    <main className="relative flex min-h-screen flex-col items-center justify-between py-0 px-2 md:px-24">
+      <section className="hero flex flex-col items-center text-center px-6 py-12 md:px-12">
         <h1 className="text-4xl font-bold text-text mb-6">Abyiss Dashboard</h1>
         <Analytics transactions={transactions} />
         <TransactionTable transactions={transactions} />
